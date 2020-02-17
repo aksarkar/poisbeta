e688224acdcb22ed16c02e089029fe278b97b7c8
Abhishek Sarkar <aksarkar@alum.mit.edu>
2020-02-17 04:49:40 -0600

    Add test cases for MCMC

--- src/poisbeta/mcmc.py
+++ src/poisbeta/mcmc.py
@@ -1,14 +1,20 @@
 import numpy as np
+import scipy.special as sp
 import scipy.stats as st
 
-def _slice_sample(f, init, width=0.1, max_steps=100, **kwargs):
+def slice_sample(f, init, width=0.1, max_steps=100, bounds=None, **kwargs):
   """Return samples from the density proportional to exp(f)
 
   f - log density (up to a constant)
   init - initial value
+  width - typical width of f
+  max_steps - maximum number of times to step out
+  bounds - support of f
   kwargs - additional arguments to f
 
   """
+  if bounds is None:
+    bounds = (-np.inf, np.inf)
   # Auxiliary variable defines the slice
   z = f(init, **kwargs) - np.random.exponential()
   # Step out
@@ -16,16 +22,22 @@ def _slice_sample(f, init, width=0.1, max_steps=100, **kwargs):
   right = left + width
   left_steps = int(np.random.uniform() * max_steps)
   for _ in range(left_steps):
-    if z < f(left, **kwargs):
+    if left < bounds[0] or z < f(left, **kwargs):
+      left = np.clip(left, *bounds)
       break
+    assert bounds[0] <= left <= bounds[1]
     left -= width
+  assert bounds[0] <= left <= bounds[1]
   for _ in range(max_steps - left_steps):
-    if z < f(right, **kwargs):
+    if right > bounds[1] or z < f(right, **kwargs):
+      right = np.clip(right, *bounds)
       break
     right += width
+  assert bounds[0] <= right <= bounds[1]
   # Step in
   while right > left:
     proposal = left + np.random.uniform() * (right - left)
+    assert bounds[0] <= proposal <= bounds[1]
     if z < f(proposal, **kwargs):
       return proposal
     elif proposal < init:
@@ -34,21 +46,24 @@ def _slice_sample(f, init, width=0.1, max_steps=100, **kwargs):
       right = proposal
   raise RuntimeError('failed to find an acceptable sample')
 
+def _safe_log(x, eps=1e-8):
+  return np.log(x + eps)
+
 def _cond_logpdf_p(p, x, kr, kon, koff):
-  return (x + kon - 1) * np.log(p) + (koff - 1) * np.log(1 - p) - kr * p
+  return (x + kon - 1) * _safe_log(p) + (koff - 1) * _safe_log(1 - p) - kr * p
 
 def _cond_logpdf_kr(kr, x, p, ar, br):
-  return ((x + ar - 1) * np.log(kr) - (p + br) * kr).sum()
+  return ((x + ar - 1) * _safe_log(kr) - (p + br) * kr).sum()
 
 def _cond_logpdf_kon(kon, p, koff, aon, bon):
-  return (kon * np.log(p) + (aon - 1) * np.log(kon)
+  return (kon * _safe_log(p) + (aon - 1) * _safe_log(kon)
           - bon * kon - sp.gammaln(kon) + sp.gammaln(kon + koff)).sum()
 
-def _cond_logpdf_koff(koff, p, on, aoff, boff):
-  return (koff * np.log(1 - p) + (aoff - 1) * np.log(koff)
-          - boff * koff - sp.gammaln(koff) + sp.gammaln(on + koff)).sum()
+def _cond_logpdf_koff(koff, p, kon, aoff, boff):
+  return (koff * _safe_log(1 - p) + (aoff - 1) * _safe_log(koff)
+          - boff * koff - sp.gammaln(koff) + sp.gammaln(kon + koff)).sum()
 
-def fit_poisson_beta_mcmc(x, n_samples, ar, br, aon, bon, aoff, boff, verbose=False):
+def fit_poisson_beta_mcmc(x, n_samples, ar, br, aon, bon, aoff, boff, max_steps=100, verbose=False):
   """Return samples from the posterior p(kon, koff, kr | x)
 
   x - counts (n,)
@@ -56,9 +71,10 @@ def fit_poisson_beta_mcmc(x, n_samples, ar, br, aon, bon, aoff, boff, verbose=Fa
   ar, br - prior parameters kr ~ Gamma(ar, br)
   aon, bon - prior parameters kon ~ Gamma(aon, bon)
   aoff, boff - prior parameters koff ~ Gamma(aoff, boff)
+  max_steps - maximum number of steps in slice sampling
+  verbose - if True, print log joint after each sample
 
   """
-  samples = []
   # Important: these are fixed
   Fr = st.gamma(a=ar, scale=1 / br)
   Fon = st.gamma(a=aon, scale=1 / bon)
@@ -68,18 +84,20 @@ def fit_poisson_beta_mcmc(x, n_samples, ar, br, aon, bon, aoff, boff, verbose=Fa
   kon = Fon.rvs(size=1)
   koff = Foff.rvs(size=1)
   p = st.beta(a=kon, b=koff).rvs(size=x.shape)
+  samples = []
   for t in range(n_samples):
     samples.append((kr, kon, koff))
     for i in range(x.shape[0]):
-      p[i] = _slice_sample(_cond_logpdf_p, init=p[i], x=x[i], kr=kr, kon=kon, koff=koff)
-    kr = _slice_sample(_cond_logpdf_kr, init=kr, x=x, p=p, ar=ar, br=br)
-    kon = _slice_sample(_cond_logpdf_kon, init=kon, p=p, koff=koff, aon=aon, bon=bon)
-    koff = _slice_sample(_cond_logpdf_koff, init=koff, p=p, kon=kon, aoff=aoff, boff=boff)
+      p[i] = slice_sample(_cond_logpdf_p, init=p[i], x=x[i], kr=kr, kon=kon, koff=koff, bounds=[1e-8, 1 - 1e-8])
+    kr = slice_sample(_cond_logpdf_kr, init=kr, x=x, p=p, ar=ar, br=br, bounds=[1e-8, np.inf])
+    kon = slice_sample(_cond_logpdf_kon, init=kon, p=p, koff=koff, aon=aon, bon=bon, bounds=[1e-8, np.inf])
+    koff = slice_sample(_cond_logpdf_koff, init=koff, p=p, kon=kon, aoff=aoff, boff=boff, bounds=[1e-8, np.inf])
     if verbose:
+      # TODO: this blows up for p = 0 and p = 1
       log_joint = (st.poisson(mu=kr * p).logpmf(x).sum()
                    + st.beta(a=kon, b=koff).logpdf(p).sum()
                    + Fr.logpdf(kr)
                    + Fon.logpdf(kon)
                    + Foff.logpdf(koff))
       print(f'sample {t}: {log_joint}')
-  return samples
+  return np.array(samples).reshape(-1, 3)
---
+++ tests/test_mcmc.py
@@ -0,0 +1,46 @@
+import numpy as np
+import poisbeta.mcmc
+import pytest
+import scipy.stats as st
+
+from fixtures import *
+
+@pytest.mark.xfail
+def test__slice_sample():
+  f = st.norm().logpdf
+  samples = [poisbeta.mcmc._slice_sample(f, 0, width=4) for i in range(1000)]
+  d, p = st.kstest(samples, 'norm')
+  assert p >= 0.05
+
+def test__conf_logpdf_p():
+  grid = np.linspace(0, 1, 1000)
+  f = poisbeta.mcmc._cond_logpdf_p(p=grid, x=10, kr=5, kon=.5, koff=.5)
+  assert np.isfinite(f).all()
+
+def test__conf_logpdf_kr():
+  grid = np.logspace(-3, 1, 1000)
+  f = poisbeta.mcmc._cond_logpdf_kr(kr=grid, x=10, p=.5, ar=10, br=1)
+  assert np.isfinite(f).all()
+
+def test__conf_logpdf_kon():
+  grid = np.logspace(-3, 1, 1000)
+  f = poisbeta.mcmc._cond_logpdf_kon(kon=grid, p=.5, koff=.5, aon=1, bon=100)
+  assert np.isfinite(f).all()
+
+def test__conf_logpdf_koff():
+  grid = np.logspace(-3, 1, 1000)
+  f = poisbeta.mcmc._cond_logpdf_koff(koff=grid, p=.5, kon=.5, aoff=1, boff=100)
+  assert np.isfinite(f).all()
+
+@pytest.mark.xfail
+def test_fit_pois_beta_mcmc_one_sample(simulate_easy):
+  x, *theta = simulate_easy
+  samples = poisbeta.mcmc.fit_poisson_beta_mcmc(x, n_samples=1, ar=x.max(), br=x.max(), aon=1, bon=100, aoff=1, boff=100)
+  assert samples.shape == (1, 3)
+
+@pytest.mark.xfail
+def test_fit_pois_beta_mcmc_n_samples(simulate_easy):
+  x, *theta = simulate_easy
+  n_samples = 2
+  samples = poisbeta.mcmc.fit_poisson_beta_mcmc(x, n_samples=n_samples, ar=x.max(), br=x.max(), aon=1, bon=100, aoff=1, boff=100)
+  assert samples.shape == (n_samples, 3)
